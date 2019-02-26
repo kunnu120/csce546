d7bff71da813f4b0af6f76c467b6e2a8817b6e14
maheedhar1998 <maheedhar1998@hotmail.com>
2019-02-26 18:20:31 -0500

    Update finished hw2

--- Homework2/restaurant/src/app/order-detail/order-detail.page.ts
+++ Homework2/restaurant/src/app/order-detail/order-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { order } from '../orders/orders.page';
+import { order, orders } from '../orders/orders.page';
 
 @Component({
   selector: 'app-order-detail',
@@ -9,11 +9,18 @@ import { order } from '../orders/orders.page';
 })
 export class OrderDetailPage implements OnInit {
   currOrder: order;
+  Orders: orders;
   constructor(private route: Router, private r: ActivatedRoute) {
     this.r.params.subscribe(params => {this.currOrder = JSON.parse(params['selectedOrder']);});
   }
 
   ngOnInit() {
+  }
+  newOrder() {
+    this.Orders = JSON.parse(localStorage.getItem('orders'));
+    this.Orders.orderList.push(new order(0));
+    this.Orders.currentOrder = this.Orders.orderList[this.Orders.orderList.length-1];
+    localStorage.setItem('orders', JSON.stringify(this.Orders));
   }
   goBack() {
     this.route.navigate(['/orders']);
