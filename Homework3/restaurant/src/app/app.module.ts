36560bbbda3113421ece1c213f75c062d033caeb
maheedhar1998 <maheedhar1998@hotmail.com>
2019-03-09 20:43:57 -0500

    Firebase Error

--- Homework3/restaurant/src/app/app.module.ts
+++ Homework3/restaurant/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { orders } from './orders/orders.page';
 import { Item } from './list/list.page';
-// import * as firebase from 'firebase';
+// import { firebase } from 'firebaseui-angular';
+import * as firebase from 'Firebase';
 
 @NgModule({
   declarations: [AppComponent],
@@ -36,18 +37,18 @@ export class AppModule {
   public dessertItems: Array<Item>;
   public otherItems: Array<Item>;
   constructor() {
-    // var config = {
-    //   apiKey: "AIzaSyChZSnSBcwWGQv2GAf4VBIhyB0P4CjkHtI",
-    //   authDomain: "restaurant-16ab5.firebaseapp.com",
-    //   databaseURL: "https://restaurant-16ab5.firebaseio.com",
-    //   projectId: "restaurant-16ab5",
-    //   storageBucket: "restaurant-16ab5.appspot.com",
-    //   messagingSenderId: "405504601979"
-    // };
-    // firebase.initializeApp(config);
+    var config = {
+      apiKey: "AIzaSyChZSnSBcwWGQv2GAf4VBIhyB0P4CjkHtI",
+      authDomain: "restaurant-16ab5.firebaseapp.com",
+      databaseURL: "https://restaurant-16ab5.firebaseio.com",
+      projectId: "restaurant-16ab5",
+      storageBucket: "restaurant-16ab5.appspot.com",
+      messagingSenderId: "405504601979"
+    };
+    firebase.initializeApp(config);
     this.listOfOrders = new orders();
     localStorage.setItem('orders', JSON.stringify(this.listOfOrders));
-    // firebase.database().ref('orders').push().set(JSON.stringify(this.listOfOrders));
+    firebase.database().ref('orders').push().set(JSON.stringify(this.listOfOrders));
     this.breakfastItems = [];
     this.lunchItems = [];
     this.dinnerItems = [];
