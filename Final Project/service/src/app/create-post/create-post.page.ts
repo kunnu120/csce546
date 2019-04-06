349f8fb8b7335945e6d860b554c6bffcf2c18831
maheedhar1998 <maheedhar1998@hotmail.com>
2019-04-06 04:58:17 -0400

    Update create post page

--- Final Project/service/src/app/create-post/create-post.page.ts
+++ Final Project/service/src/app/create-post/create-post.page.ts
@@ -8,8 +8,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-post.page.scss'],
 })
 export class CreatePostPage implements OnInit {
-
-  constructor(private route: Router) { }
+  images: string[];
+  title: string;
+  price: number;
+  description: string;
+  location: string;
+  constructor(private route: Router) {
+    this.title = "";
+    this.description = "";
+    this.location = "";
+    this.images = [];
+    this.images.push("https://i.ytimg.com/vi/jsAoS2ghTxk/maxresdefault.jpg");
+    this.images.push("https://happyfm.es/wp-content/uploads/2019/01/v-bts-publica-scenery-la-cancion-mas-especial-de-su-vida-01.jpg");
+    this.images.push("https://media.allure.com/photos/5b76eb54b60c70133b1eaac3/master/pass/BTS%20V%20mini%20ponytail%20hair.jpg");
+  }
 
   ngOnInit() {
   }
@@ -19,6 +31,9 @@ export class CreatePostPage implements OnInit {
   makePost() {
 
   }
+  addImages() {
+
+  }
 }
 export class Post {
   public images: string[];
@@ -26,4 +41,19 @@ export class Post {
   public price: number;
   public description: string;
   public location: string;
+  public active: boolean;
+  constructor(imgs: string[], ttl: string, prc: number, dscr: string, loc: string) {
+    this.images = [];
+    for(var i: number=0; i<imgs.length; i++) {
+      this.images.push(imgs[i]);
+    }
+    this.title = ttl;
+    this.price = prc;
+    this.description = dscr;
+    this.location = loc;
+    this.active = true;
+  }
+  deactivate() {
+    this.active = false;
+  }
 }
